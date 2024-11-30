04e0cf8c2ca1c8b0c192325e3e7cf6ead47f1cdb
Brandon Stilson <bbgithub@fastmail.com>
2024-11-29 23:21:23 -0800

    s3 sdk example

--- src/main.rs
+++ src/main.rs
@@ -2,13 +2,78 @@ pub mod backblaze_api;
 pub mod consts;
 pub mod models;
 
-use std::{path::PathBuf, sync::Arc, time::Duration};
+use std::{
+    fs::File,
+    io::Write,
+    path::PathBuf,
+    sync::Arc,
+    time::{Duration, Instant},
+};
 
 use backblaze_api::BackblazeApi;
 use rand::seq::SliceRandom;
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
+    // pick one
+    // native_api_example().await?;
+    // s3_sdk_example().await?;
+
+    Ok(())
+}
+
+async fn s3_sdk_example() -> anyhow::Result<()> {
+    let config = aws_config::load_from_env().await;
+    let client = aws_sdk_s3::Client::new(&config);
+
+    let r = client
+        .list_objects_v2()
+        .bucket("bbstilson-test-town")
+        .max_keys(1000)
+        .prefix("audio")
+        .send()
+        .await?;
+    let keys = r
+        .contents()
+        .into_iter()
+        .filter_map(|c| c.key())
+        .collect::<Vec<_>>();
+
+    let downloads = 100;
+    let mut durations = vec![Duration::default(); downloads];
+    for idx in 0..downloads {
+        let key = keys.choose(&mut rand::thread_rng()).unwrap();
+        let path = PathBuf::from(key);
+
+        let start = Instant::now();
+        let r = client
+            .get_object()
+            .bucket("bbstilson-test-town")
+            .key(*key)
+            .send()
+            .await?;
+
+        let mut f = File::create(path)?;
+        let mut data = r.body;
+        while let Some(bytes) = data.try_next().await? {
+            f.write_all(&bytes)?;
+        }
+        durations[idx] = start.elapsed();
+    }
+
+    println!("{durations:?}");
+
+    let total = durations
+        .into_iter()
+        .map(|d| d.as_millis() as f32)
+        .sum::<f32>();
+    let avg = total / (downloads as f32);
+    println!("average duration: {:?}", avg);
+
+    Ok(())
+}
+
+async fn native_api_example() -> anyhow::Result<()> {
     let api = Arc::new(BackblazeApi::new().await?);
     // upload_files(api, 100).await?;
     let files = api.list_files().await?;
@@ -28,7 +93,6 @@ async fn main() -> anyhow::Result<()> {
         .sum::<f32>();
     let avg = total / (downloads as f32);
     println!("average duration: {:?}", avg);
-
     Ok(())
 }
 
